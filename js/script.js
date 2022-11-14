1c6dbbbec14e4f32194c3b53959239a6e9c309ef
Chris Booth <31346759+chrisPhat@users.noreply.github.com>
2022-11-14 16:05:09 +0000

    added validation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('Script Loaded');
+var signUpForm = document.querySelector('#signUpForm');
+var userNameField = document.querySelector('#usernameField');
+var userNameErrorMessage = document.querySelector('#usernameErrorMessage');
+
+signUpForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+
+    if(!signUpForm.username.value){
+        userNameField.classList.add('errorField');
+        userNameErrorMessage.classList.remove('hidden');
+        console.log('You need to enter a username');
+    }
+
+
+});
